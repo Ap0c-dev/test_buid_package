e7e977e25cc3595f5ddc2e9cbcee06229b222139
enacom <tiago.barros@estudante.iftm.edu.br>
2023-01-22 15:05:54 -0300

    first commit

---
+++ R/soma.R
@@ -0,0 +1,10 @@
+#' Função Somar
+#' A função realiza a soma de dois números.
+#' @param a recebe a primeira parcela
+#' @param b recebe a segunda parcela
+#' @examples
+#' somar(3,4)
+#'@export
+somar <- function(a, b){
+  a + b
+}
---
+++ R/subtracao.R
@@ -0,0 +1,10 @@
+#' Função Subtrair
+#' A função realiza a subtraição de dois números.
+#' @param a recebe a primeira parcela
+#' @param b recebe a segunda parcela
+#' @examples
+#' subtrair(3,4)
+#' @import dplyr
+subtrair <- function(a, b){
+  a - b
+}
